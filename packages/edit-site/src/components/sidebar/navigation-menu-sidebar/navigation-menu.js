f4216d1ec6b1ad10cbac901123fb52a8b65e03bb
Daniel Richards <daniel.richards@automattic.com>
2022-05-03 15:38:03 +0800

    Remove showNestedBlocks

--- packages/edit-site/src/components/sidebar/navigation-menu-sidebar/navigation-menu.js
+++ packages/edit-site/src/components/sidebar/navigation-menu-sidebar/navigation-menu.js
@@ -50,7 +50,7 @@ export default function NavigationMenu( { innerBlocks, id } ) {
 	}, [ updateBlockListSettings, innerBlocks ] );
 	return (
 		<>
-			<ListView id={ id } showNestedBlocks expandNested={ false } />
+			<ListView id={ id } expandNested={ false } />
 		</>
 	);
 }
